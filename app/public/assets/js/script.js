b305dc1fd99bb2ebb173f3766c96f89acc21522b
denise martin <denise.martin@vanderbilt.edu>
2019-04-30 15:48:46 -0500

    moved friends.js and modified script.js, server.js and survey.html

--- app/public/assets/js/script.js
+++ app/public/assets/js/script.js
@@ -1,8 +1,47 @@
 
 var newFriend = {};
+var possibleOne = {};
+var possibleTwo = {};
+var compare = 0;
 
 $("#submit").on("click", function () {
     newFriend = {
-        name: $("input[name='name']")
+        name: $("input[name='name']").val(),
+        photo: $("input[name='photo']").val(),
+        scores: []
     }
+
+    var answers = $('input:checked');
+    for (let i = 0; i < answers.length; i++) {
+        newFriend.scores.push(answers[i].value);
+    }
+
+    for (let f = 0; f < friends.length; f++) {
+
+        if(possibleTwo) {
+            possibleOne.name = f;
+            
+            for (let i = 0; i < newFriend.scores.length; i++) {
+                compare =+ Math.abs (parseInt(friends[f].scores[i]) - parseInt(newFriend.scores[i]));
+            }
+            possibleOne.value = compare;
+
+            if(possibleOne.value < possibleTwo.value) {
+                possibleTwo = {};
+            } else {
+                possibleOne = possibleTwo;
+                possibleTwo = {};
+            }
+            
+        } else { 
+            possibleTwo.name = f;
+            for (let i = 0; i < newFriend.scores.length; i++) {
+                compare =+ Math.abs (parseInt(friends[f].scores[i]) - parseInt(newFriend.scores[i]));
+        };
+        possibleTwo.value = compare;
+    }
+}
+
 })
+
+
