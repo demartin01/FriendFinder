a5ed453d65f9775d7dad293547d5628cf79a3bf6
denise martin <denise.martin@vanderbilt.edu>
2019-05-03 14:36:55 -0500

    removed node_modules

--- app/public/assets/js/script.js
+++ app/public/assets/js/script.js
@@ -1,46 +1,72 @@
 
-var newFriend = {};
-var possibleOne = {};
+var newFriend;
+var possibleOne;
 var possibleTwo = {};
 var compare = 0;
+
 
 $("#submit").on("click", function () {
     newFriend = {
         name: $("input[name='name']").val(),
         photo: $("input[name='photo']").val(),
         scores: []
-    }
+    };
 
     var answers = $('input:checked');
-    for (let i = 0; i < answers.length; i++) {
-        newFriend.scores.push(answers[i].value);
-    }
 
-    for (let f = 0; f < friends.length; f++) {
+    if (answers.length === 10) {    
 
-        if(possibleTwo) {
-            possibleOne.name = f;
-            
+        for (let i = 0; i < answers.length; i++) {
+            newFriend.scores.push(answers[i].value);
+        }
+        console.log("New Friend: ", newFriend);
+
+        for (let f = 0; f < friends.length; f++) {
+            console.log("Friends index: ", f);
+
+            if (possibleOne === undefined) {
+                possibleOne = {};
+                possibleOne.name = f;
+                for (let i = 0; i < newFriend.scores.length; i++) {
+                    compare += Math.abs(parseInt(friends[f].scores[i]) - parseInt(newFriend.scores[i]));
+
+                };
+                possibleOne.value = compare;
+
+                console.log("possibleOne Value: ", possibleOne.value);
+                console.log("possibleOne", friends[possibleOne.name])
+                compare = 0;
+
+                f++;
+
+            };
+
+            possibleTwo.name = f;
+            console.log("Friends index2: ", f);
+
             for (let i = 0; i < newFriend.scores.length; i++) {
-                compare =+ Math.abs (parseInt(friends[f].scores[i]) - parseInt(newFriend.scores[i]));
+                compare += Math.abs(parseInt(friends[f].scores[i]) - parseInt(newFriend.scores[i]));
             }
-            possibleOne.value = compare;
-
-            if(possibleOne.value < possibleTwo.value) {
+            possibleTwo.value = compare;
+            compare = 0;
+            console.log("possibleTwo Value: ", possibleTwo.value);
+            console.log("possibleTwo", friends[possibleTwo.name])
+            if (possibleOne.value < possibleTwo.value) {
                 possibleTwo = {};
             } else {
                 possibleOne = possibleTwo;
                 possibleTwo = {};
-            }
-            
-        } else { 
-            possibleTwo.name = f;
-            for (let i = 0; i < newFriend.scores.length; i++) {
-                compare =+ Math.abs (parseInt(friends[f].scores[i]) - parseInt(newFriend.scores[i]));
+            };
         };
-        possibleTwo.value = compare;
-    }
-}
+
+        const yourFriend = $("<img>", {"src": friends[possibleOne.name].image})
+
+        $(".modal-body").html(yourFriend);
+        $(".modal-footer").prepend(`<h2 style={"margin-left": "5px"}>${friends[possibleOne.name].name}</h2>`);
+
+        console.log(friends[possibleOne.name]);
+
+    } else { alert("Please answer all the questions.") }
 
 })
 
